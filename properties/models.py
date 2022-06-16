6024da3c0cc25904850c2ba59f2020663bd51cf5
tdawes93 <tdawes136@googlemail.com>
2022-06-16 18:04:09 +0000

    Add initial property class and property test class structure

--- properties/models.py
+++ properties/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Property(models.Model):
+    """
+    A class to represnt a property eligible for review.
+    """
+    title = models.CharField(max_length=100)
