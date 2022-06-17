bb0177a33201193d6617ca3ed2684277ed1507b5
tdawes93 <tdawes136@googlemail.com>
2022-06-17 19:11:29 +0000

    Add first unit test for property model

--- properties/tests/tests_models.py
+++ properties/tests/tests_models.py
@@ -6,4 +6,21 @@ class PropertyTest(TestCase):
     """
     Test class to test the Property class found in properties.models
     """
-    
+    def setUp(self):
+        self.property1 = Property.objects.create(
+            title='Property 1',
+            address='5 Firgrove Hill, Farnham, Surrey, GU9 8LH',
+            num_of_bedrooms=4,
+            num_of_bathrooms=3,
+            type_of_property=1,
+            for_rent=False,
+            )
+
+    def test_title_has_max_length_of_100(self):
+        """
+        If property1 title is less than 100 characters test will return True
+        """
+        self.assertTrue(
+            len(self.property1.title) < 101,
+            len('Property 1') < 101
+            )
