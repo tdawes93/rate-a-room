225aceec8be32fe53eec129ed9ec8075f201aa6a
tdawes93 <tdawes136@googlemail.com>
2022-06-20 13:44:14 +0000

    Add property class to admin with search field an pre-pop slug

--- properties/admin.py
+++ properties/admin.py
@@ -4,5 +4,8 @@ from .models import Property
 
 @admin.register(Property)
 class PropertyAdmin(admin.ModelAdmin):
+    """
+    An admin class for the property model
+    """
     prepopulated_fields = {'slug': ('title',)}
     search_fields = ['title']
