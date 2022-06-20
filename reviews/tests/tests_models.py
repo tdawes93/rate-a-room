148b4f65e621224421f1fe505208c5f45acb5ab1
tdawes93 <tdawes136@googlemail.com>
2022-06-20 19:40:40 +0000

    Add unittest to check overall_rating calculates average of ratings correctly

--- reviews/tests/tests_models.py
+++ reviews/tests/tests_models.py
@@ -1,8 +1,32 @@
 from django.test import TestCase
-from properties.models import Property
+from django.contrib.auth.models import User
+from reviews.models import Review, Property
 
 
-class PropertyTest(TestCase):
+class ReviewTest(TestCase):
     """
-    Test class to test the Property class found in properties.models
+    Test class to test the Review class found in reviews.models
     """
+    def setUp(self):
+        user = User.objects.create(username='testuser')
+        property = Property.objects.create(
+            title='Test Property',
+            address_postcode='AB12CD',
+            type_of_property=1,
+            ll_or_ea=user
+        )
+        self.review1 = Review.objects.create(
+            title='Review 1',
+            condition_of_property=2,
+            quality_of_landlord=4,
+            rate_the_neighbourhood=5,
+            value_for_money=4,
+            standard_of_amenities_nearby=1,
+            user=user,
+            property=property,
+        )
+
+    def test_overall_rating_is_integer_average_of_ratings(self):
+        self.assertTrue(0 <= self.review1.overall_rating <= 5)
+        print(self.review1.overall_rating)
+        self.assertEqual(int(self.review1.overall_rating), 3)
