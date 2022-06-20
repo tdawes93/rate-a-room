5ed6e597a72a0378c76926303abcabe74b00a53a
tdawes93 <tdawes136@googlemail.com>
2022-06-20 13:44:34 +0000

    Add review class to admin with search field an pre-pop slug

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    """
+    An admin class for the Review model
+    """
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['title']
