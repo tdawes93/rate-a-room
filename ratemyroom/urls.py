ad3011894ccf1c95c854008439d1e48cb3932d45
tdawes93 <tdawes136@googlemail.com>
2022-06-22 23:39:53 +0000

    Hook up static files to urls

--- ratemyroom/urls.py
+++ ratemyroom/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from . import views
 
@@ -8,3 +9,5 @@ urlpatterns = [
     path('', views.PropertyList.as_view(), name='homepage'),
     path('properties/', include('properties.urls'), name='property_urls'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
