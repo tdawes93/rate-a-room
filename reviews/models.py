045728f1fc4e7e8bab3879de45885a436a6eeb96
tdawes93 <tdawes136@googlemail.com>
2022-06-23 13:14:26 +0000

    Add default to rating fields in model and migrate

--- reviews/models.py
+++ reviews/models.py
@@ -24,19 +24,24 @@ class Review(models.Model):
     date_rented_to = models.DateField(null=True, blank=True)
     content = models.TextField(max_length=500, null=True, blank=True)
     condition_of_property = models.PositiveIntegerField(
-        validators=[MaxValueValidator(5)]
+        validators=[MaxValueValidator(5),],
+        default=0,
     )
     quality_of_landlord = models.PositiveIntegerField(
-        validators=[MaxValueValidator(5)]
+        validators=[MaxValueValidator(5)],
+        default=0,
     )
     rate_the_neighbourhood = models.PositiveIntegerField(
-        validators=[MaxValueValidator(5)]
+        validators=[MaxValueValidator(5)],
+        default=0,
     )
     value_for_money = models.PositiveIntegerField(
-        validators=[MaxValueValidator(5)]
+        validators=[MaxValueValidator(5)],
+        default=0,
     )
     standard_of_amenities_nearby = models.PositiveIntegerField(
-        validators=[MaxValueValidator(5)]
+        validators=[MaxValueValidator(5)],
+        default=0,
     )
     overall_rating = models.PositiveIntegerField(blank=True, null=True)
     date_reviewed = models.DateTimeField(auto_now_add=True)
