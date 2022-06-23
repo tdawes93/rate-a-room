ffb9a1e9613d4179552d84e7c932e7b7c66a080b
tdawes93 <tdawes136@googlemail.com>
2022-06-23 20:56:09 +0000

    Edit js code to fix multiple star selected bug

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,3 @@
-
 // Code from taken and edited from Pyplane Youtube channel
 
 //get all the stars
@@ -12,10 +11,10 @@ const form = document.querySelector('.rate-form')
 const csrf = document.getElementsByName('csrfmiddlewaretoken');
 
 const handleStarSelect = (size) => {
-    const children = form.children
+    const children = document.querySelector('.rate-btn')
     console.log(children[0])
-    for (let i=0; i < children.length; i++) {
-        if(i <= size) {
+    for (let i = 0; i < children.length; i++) {
+        if (i <= size) {
             children[i].classList.add('checked')
         } else {
             children[i].classList.remove('checked')
@@ -24,33 +23,81 @@ const handleStarSelect = (size) => {
 }
 
 const handleSelect = (selection) => {
-    switch(selection) {
-        case 'first':{
-            handleStarSelect(1)
-            return
+    switch (selection) {
+        case 'first': {
+            one.classList.add('checked');
+            two.classList.remove('checked');
+            three.classList.remove('checked');
+            four.classList.remove('checked');
+            five.classList.remove('checked');
+            return;
         }
-        case 'second':{
-            handleStarSelect(2)
-            return
+        case 'second': {
+            one.classList.add('checked');
+            two.classList.add('checked');
+            three.classList.remove('checked');
+            four.classList.remove('checked');
+            five.classList.remove('checked');
+            return;
         }
-        case 'third':{
-            handleStarSelect(3)
-            return
+        case 'third': {
+            one.classList.add('checked');
+            two.classList.add('checked');
+            three.classList.add('checked');
+            four.classList.remove('checked');
+            five.classList.remove('checked');
+            return;
         }
-        case 'fourth':{
-            handleStarSelect(4)
-            return
+        case 'fourth': {
+            one.classList.add('checked');
+            two.classList.add('checked');
+            three.classList.add('checked');
+            four.classList.add('checked');
+            five.classList.remove('checked');
+            return;
         }
-        case 'fifth':{
-            handleStarSelect(5)
-            return
+        case 'fifth': {
+            one.classList.add('checked');
+            two.classList.add('checked');
+            three.classList.add('checked');
+            four.classList.add('checked');
+            five.classList.add('checked');
+            return;
         }
     }
+}
+
+const getNumericValue = (stringValue) => {
+    let numericValue;
+    if (stringValue === 'first') {
+        numericValue = 1;
+    } else if (stringValue === 'second') {
+        numericValue = 2;
+    } else if (stringValue === 'third') {
+        numericValue = 3;
+    } else if (stringValue === 'fourth') {
+        numericValue = 4;
+    } else if (stringValue === 'fifth') {
+        numericValue = 5;
+    } else {
+        numericValue = 0;
+    }
+    return numericValue;
 }
 
 
 const arr = [one, two, three, four, five];
 
-arr.forEach(item => item.addEventListener('mouseover', (event)=>{
+arr.forEach(item => item.addEventListener('mouseover', (event) => {
     handleSelect(event.target.id)
+}));
+
+arr.forEach(item => item.addEventListener('click', (event) => {
+    const val = event.target.id
+
+    form.addEventListener('submit', e => {
+        e.preventDefault
+        const val_num = getNumericValue(val);
+        alert(val_num)
+    })
 }));
