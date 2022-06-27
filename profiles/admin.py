7561867cacab8056aa87c59d6d8d3152273ccc76
tdawes93 <tdawes136@googlemail.com>
2022-06-27 02:00:05 +0000

    Add user admin class and register

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """
+    An admin class for the User model
+    """
+    search_fields = ('username',)
+    list_display = ('username', 'email')
+    readonly_fields = ('username',)
