945671b05eb6504ef1c8520e721ee6cca2624af1
tdawes93 <tdawes136@googlemail.com>
2022-06-29 02:38:46 +0000

    Pass all files through PEP8, W3C checker, add code comments annd remove files not used

--- static/js/teaser.js
+++ static/js/teaser.js
@@ -4,12 +4,14 @@
 $(function() {
 	$('.teaser').each(function() {
 		var el = $(this);
-		var long_text = el.html()
+		var long_text = el.html();
 		var n_sentences = el.attr("data-teaser-length") || 1;
-		var short_text = el.html().split(/([\.\?\!])\s/, (n_sentences * 2) ).map(function(d, i) {return i % 2 == 0 ? d : d + " " }).join("")
+		var short_text = el.html().split(/([\.\?\!])\s/, (n_sentences * 2)).map(function(d, i) {
+			return i % 2 == 0 ? d : d + " "
+		}).join("");
 
 		if (long_text != short_text) {
-			el.html('')
+			el.html('');
 			el.append(
 				"<span class='teaser-long'>" +
 				long_text +
@@ -19,16 +21,24 @@ $(function() {
 				"<span class='teaser-see-more' style='cursor:pointer;margin-top:5px;display:block;' " +
 				"onclick='$(this).parent().hide();$(this).parent().siblings(\".teaser-long\").show();'> See more...</span>" +
 				"</span>"
-				)
-			el.children('.teaser-long').hide()
+			);
+			el.children('.teaser-long').hide();
 		}
-	})
+	});
 
 	$('.teaser-see-more')
-		.mouseenter(function() {$(this).css("text-decoration", "underline")})
-		.mouseenter(function() {$(this).css("color", "#A5A640")})
-		.mouseleave(function() {$(this).css("text-decoration", "none")})
-		.mouseleave(function() {$(this).css("color", "#171C1F")})
+		.mouseenter(function() {
+			$(this).css("text-decoration", "underline");
+		})
+		.mouseenter(function() {
+			$(this).css("color", "#A5A640");
+		})
+		.mouseleave(function() {
+			$(this).css("text-decoration", "none");
+		})
+		.mouseleave(function() {
+			$(this).css("color", "#171C1F");
+		});
 
 
-})
+});
