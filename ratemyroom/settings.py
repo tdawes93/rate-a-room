7ec920600c159d5c75de907246bfa0bee246c685
tdawes93 <tdawes136@googlemail.com>
2022-11-28 20:49:01 +0000

    Add settings to migrate deployment from Heroku to Render

--- ratemyroom/settings.py
+++ ratemyroom/settings.py
@@ -36,6 +36,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['rate-my-room.herokuapp.com', 'localhost']
 
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
